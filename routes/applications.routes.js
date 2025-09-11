affcbf0a64ba7af8aae81e7cc72fa85d8ced9fa3
Lucas-Ballu <lucas0999.lb@gmail.com>
2025-09-12 00:38:52 +0200

    FEAT: ajout du middlewares aux routes apps et users

--- routes/applications.routes.js
+++ routes/applications.routes.js
@@ -1,11 +1,17 @@
 import Router from "express";
 import auth from "../middlewares/auth.middleware.js";
 import { ApplicationsController } from "../controller/applications.controller.js";
+import { authorizeRole } from "../middlewares/roles.middleware.js";
 
 const appRouter = Router();
 const appCtrl = new ApplicationsController();
 
-appRouter.post("/applications", auth, (req, res) => appCtrl.apply(req, res));
+appRouter.post(
+  "/applications",
+  auth,
+  authorizeRole(["VOLUNTEER"]),
+  (req, res) => appCtrl.apply(req, res)
+);
 
 appRouter.get("/applications/myApplications", auth, (req, res) =>
   appCtrl.myApplications(req, res)
--- routes/missions.routes.js
+++ routes/missions.routes.js
@@ -1,12 +1,16 @@
 import Router from "express";
 import auth from "../middlewares/auth.middleware.js";
 import { MissionController } from "../controller/missions.controller.js";
+import { authorizeRole } from "../middlewares/roles.middleware.js";
 
 const missionRouter = Router();
 const missionCtrl = new MissionController();
 
-missionRouter.post("/missions", auth, (req, res) =>
-  missionCtrl.create(req, res)
+missionRouter.post(
+  "/missions",
+  auth,
+  authorizeRole(["ASSOCIATION"]),
+  (req, res) => missionCtrl.create(req, res)
 );
 
 missionRouter.get("/missions", (req, res) => missionCtrl.list(req, res));
